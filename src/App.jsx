9b9825dda9329e80c9a3a3bfd1c69be9d27e6ad6
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-09-05 20:28:10 +0300

    Reviews Project Finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import people from './data'
 import { FaChevronCircleLeft, FaChevronCircleRight, FaQuoteRight } from 'react-icons/fa'
 
-const App = () => {
+const App = ()=> {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index]
 
