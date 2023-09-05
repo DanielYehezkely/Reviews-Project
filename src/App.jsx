68943bcd06cabccaa39a3a335066ca4ccfb0cece
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-09-05 20:23:06 +0300

    Adding random person Button

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,14 @@ const checkNumber = (number) => {
     return checkNumber(newIndex);
   }) };
 
+const randomPerson = () => {
+   let randomNumber = Math.floor(Math.random() * people.length);
+   if(randomNumber === index){
+    randomNumber = index + 1
+   }
+   setIndex(checkNumber(randomNumber));
+}
+
   return (
     <main>
       <article className="review" >
@@ -54,6 +62,9 @@ const checkNumber = (number) => {
           <FaChevronCircleRight />
           </button>
         </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          surprise me
+        </button>
       </article>
     </main>
   );
