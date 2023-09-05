46f68ae5d2ccae9d30bce88e65776001fd91c812
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-09-05 20:01:30 +0300

    Next and Prev button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index]
 
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return newIndex;
+    })
+   };
+  const prevPerson = () => { setIndex((currentIndex) => {
+    const newIndex = currentIndex - 1;
+    return newIndex;
+  }) };
+
   return (
     <main>
       <article className="review" >
@@ -18,6 +29,14 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronCircleLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+          <FaChevronCircleRight />
+          </button>
+        </div>
       </article>
     </main>
   );
